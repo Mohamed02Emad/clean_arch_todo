d0419aa45674cfd4265a92d845862db553922229
Mohamed Emad <aleslam12345@gmail.com>
2023-07-11 01:32:33 +0300

    add todo button

--- app/src/main/java/com/motodo/todo/presentation/fragmentHome/HomeFragment.kt
+++ app/src/main/java/com/motodo/todo/presentation/fragmentHome/HomeFragment.kt
@@ -5,18 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
-import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
-import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
-import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
-import com.michalsvec.singlerowcalendar.utils.DateUtils
-import com.motodo.todo.R
 import com.motodo.todo.databinding.FragmentHomeBinding
 import com.motodo.todo.presentation.recyclerViews.TodosAdapter
-import com.motodo.todo.utils.DateHelper
 import java.util.Date
 
 class HomeFragment : Fragment() {
@@ -38,53 +31,34 @@ class HomeFragment : Fragment() {
         setupRecyclerView()
         setupRowCalendar()
         setObservers()
+        setOnClickListeners()
+    }
 
+    private fun setOnClickListeners() {
+        binding.btnAddNewTodo.setOnClickListener {
+            Toast.makeText(requireContext(), "Add new task", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setupRecyclerView() {
         myAdapter = TodosAdapter()
         myAdapter.differ.submitList(viewModel.todos.value)
         binding.rvTodos.adapter = myAdapter
-
     }
 
     private fun setObservers() {
      viewModel.todos.observe(viewLifecycleOwner){newList ->
          myAdapter.differ.submitList(newList)
-         //Toast.makeText(requireContext(), "Observers", Toast.LENGTH_SHORT).show()
      }
     }
 
     val myCalendarChangesObserver = object : CalendarChangesObserver {
-        override fun whenWeekMonthYearChanged(
-            weekNumber: String,
-            monthNumber: String,
-            monthName: String,
-            year: String,
-            date: Date
-        ) {
-            super.whenWeekMonthYearChanged(weekNumber, monthNumber, monthName, year, date)
-        }
-
         override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
             super.whenSelectionChanged(isSelected, position, date)
             if (viewModel.isNewDate(date)) {
                 viewModel.dayChanged(date)
             }
         }
-
-        override fun whenCalendarScrolled(dx: Int, dy: Int) {
-            super.whenCalendarScrolled(dx, dy)
-        }
-
-        override fun whenSelectionRestored() {
-            super.whenSelectionRestored()
-        }
-
-        override fun whenSelectionRefreshed() {
-            super.whenSelectionRefreshed()
-        }
-
     }
 
     private fun setupRowCalendar() {
